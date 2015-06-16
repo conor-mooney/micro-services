1794a14c4de36a6003d3e9b78b80d5f472e2f97a
Conor Mooney <conor.mooney@gmail.com>
2015-06-16 15:24:14 +0100

    basic express service working (browser only)

--- express/routes/users.js
+++ express/routes/users.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/*
+ * GET userlist.
+ */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('userlist');
+    console.log('loading data from db ...');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
 });
 
 module.exports = router;
+
