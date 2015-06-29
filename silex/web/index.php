5fcc319c2339e16a19a9fe5203fed07ecb2c879c
Conor Mooney <conor.mooney@gmail.com>
2015-06-29 19:15:22 +0100

    silex people end point working

--- silex/web/index.php
+++ silex/web/index.php
@@ -1,5 +1,46 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Response;
+
+
+$people = [
+    [
+        "first name" =>  "Bart",
+        "id"         =>  1,
+        "last name"  =>  "Simpson"
+    ],
+    [
+        "first name" =>  "Homer",
+        "id"         =>  2,
+        "last name"  =>  "Simpson"
+    ],
+    [
+        "first name" =>  "Marge",
+        "id"         =>  3,
+        "last name"  =>  "Simpson"
+    ],
+    [
+        "first name" =>  "Lisa",
+        "id"         =>  4,
+        "last name"  =>  "Simpson"
+    ],
+    [
+        "first name" =>  "Maggy",
+        "id"         =>  5,
+        "last name"  =>  "Simpson"
+    ],
+    [
+        "first name" =>  "Donald",
+        "id"         =>  6,
+        "last name"  =>  "Duck"
+    ],
+    [
+        "first name" =>  "Daffy",
+        "id"         =>  7,
+        "last name"  =>  "Duck"
+    ]
+];
+
 
 $app = new Silex\Application();
 $app['debug'] = true;
@@ -7,8 +48,13 @@ $app['debug'] = true;
 
 $app->get('/', function() {
     $output = 'person microservice (silex)';
-
     return $output;
 });
+
+
+$app->get('/people', function() use($people, $app) {
+    return new Response($app->json($people), 201);
+});
+
 
 $app->run();
